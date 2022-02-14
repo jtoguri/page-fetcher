a895756fd55d57933eafcf0a487fd47375795d08
Joshua Toguri <joshua.toguri@gmail.com>
2022-02-14 14:26:34 -0500

    refactored to clean up the conditional based on the readline question

--- fetcher.js
+++ fetcher.js
@@ -10,35 +10,38 @@ const rl = readline.createInterface({
 const reqUrl = process.argv[2];
 const filePath = process.argv[3];
 
+if (reqUrl === undefined || filePath === undefined) {
+  console.log('Please enter a URL and file path');
+  process.exit(1);
+}
+
+const downloadAndSaveFile = (filePath, body) => {
+  fs.writeFile(filePath, body, (err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    fs.stat(filePath, (err, stats) => {
+      console.log(`Downloaded and saved ${stats.size} bytes to ${filePath}`);
+      process.exit(0);
+    });
+  });
+}
+
 request(reqUrl, function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
+  if (error) {
+    console.error(error.message); // Print the error if one occurred
+    process.exit(1);
+  }
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  if (error !== null || response.statusCode !== 200) process.exit();``
-  
   fs.stat(filePath, (err) => {
     if (err === null) {
       rl.question('This file already exists, would you like to overwrite it? (y for yes) ', (answer) => {
-        if (answer !== 'y') rl.close();
-        else {
-          fs.writeFile(filePath, body, () => {
-            fs.stat(filePath, (err, stats) => {
-              console.log(`Downloaded and saved ${stats.size} bytes to ${filePath}`);
-            });
-          });
-          rl.close();
-        }
+        rl.close();
+        if (answer === 'y') downloadAndSaveFile(filePath, body);
       });
     } else {
-      fs.writeFile(filePath, body, err => {
-        if (err) {
-          console.error(err);
-          process.exit();
-        }
-        fs.stat(filePath, (err, stats) => {
-          console.log(`Downloaded and saved ${stats.size} bytes to ${filePath}`);
-          process.exit();
-        });
-      });
+      downloadAndSaveFile(filePath, body);
     }
   });
 });
